3354013202739d76e45cb7289588eccabd042a76
Gautam <gautam@gmail.com>
2026-07-09 18:53:01 +0530

    Create factory Repository and Services for Seller

---
+++ src/services/seller.service.ts
@@ -0,0 +1,27 @@
+import { UserRepositoryFactory } from "../factories/auth.repository.factory.js";
+import { SellerRepositoryFactory } from "../factories/seller.repository.factory.js";
+import type { CreateSellerProfileInput } from "../repositories/seller.repository.js";
+import { AppError } from "../utils/appError.js";
+
+
+
+const sellerRepository = SellerRepositoryFactory.create()
+// const userRepository = UserRepositoryFactory.create()
+
+
+export const createSellerProfileService = async(data:CreateSellerProfileInput)=>{
+  const {shop_name,ifsc_code,gst_number,bank_account_number, user_id} = data;
+
+  const existingSeller = await sellerRepository.findSellerProfileByUserId(user_id)
+
+  if(existingSeller){
+    throw new AppError(409, "Seller Profile already exist")
+  }
+
+  const profile = await sellerRepository.createSellerProfile(data);
+
+
+  return profile
+
+   
+}
