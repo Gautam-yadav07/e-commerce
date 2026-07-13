555899661c477c7239c84abd2d2179a407e8e6b1
Gautam <gautam@gmail.com>
2026-07-13 18:14:04 +0530

    Define routes for Forgot password endpoint

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,10 @@
 import express from 'express'
-import {getUserProfileController, generateAccessTokenController, userLoginController, userRegisterController, logoutController } from '../controllers/auth.controller.js';
+import {getUserProfileController, generateAccessTokenController, userLoginController, userRegisterController, logoutController, forgotPasswordController, resetPasswordController } from '../controllers/auth.controller.js';
 import { authentication } from '../middlewares/authentication.middleware.js';
 import { validator } from '../middlewares/validator.middleware.js';
 import { registerSchema } from '../validators/register.schema.js';
 import { loginSchema } from '../validators/loginSchema.js';
+import { forgotPasswordSchema, resetPasswordSchema } from '../validators/auth.schema.js';
 
 const router = express.Router();
 
@@ -12,5 +13,9 @@ router.post("/login",validator(loginSchema), userLoginController)
 router.post("/refresh-token", generateAccessTokenController)
 router.get("/me",authentication, getUserProfileController)
 router.post("/logout", authentication, logoutController)
+
+router.post("/forgot-password", validator(forgotPasswordSchema), forgotPasswordController);
+
+router.post("/reset-password", validator(resetPasswordSchema), resetPasswordController);
 
 export default router;
