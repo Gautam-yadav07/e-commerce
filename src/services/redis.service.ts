92a95ac6cde92853bc3f1a46d5d2a3d1898450a7
Gautam <gautam@gmail.com>
2026-07-14 14:06:39 +0530

    function name change

--- src/services/redis.service.ts
+++ src/services/redis.service.ts
@@ -14,12 +14,13 @@ export const getCacheData = async (key: string) => {
 
   } catch (error) {
     console.log("Error getting data from redis", error)
+    return null;
   }
 
 }
 
 
-export const setCachedData = async (key: string, value: any, ttlSeconds: number = 3600) => {
+export const setCacheData = async (key: string, value: any, ttlSeconds: number = 3600) => {
   try {
     await redisClient.set(key, JSON.stringify(value))
     const data = JSON.stringify(value)
