7e1e4b95fc35dead6f49ec66320d8dd691416a88
Gautam <gautam@gmail.com>
2026-07-14 17:05:30 +0530

    Created Docker file

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,10 +4,13 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 
+// const DATABASE_URL = process.env.DATABASE_URL;
+
 const DATABASE_URL = process.env.DATABASE_URL;
 if (!DATABASE_URL) {
-   throw new Error("DATABASE_URL is missing");
+  throw new Error("DATABASE_URL is missing");
 }
+console.log(DATABASE_URL)
 
 
 export default defineConfig({
