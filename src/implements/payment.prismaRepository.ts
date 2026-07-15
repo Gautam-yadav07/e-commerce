1936583167fced751229b37e9ad2e8a8381c5e05
Gautam-yadav07 <ahirgautamyadav862004@gmail.com>
2026-07-16 00:12:39 +0530

    Add transaction in update product stock

--- src/implements/payment.prismaRepository.ts
+++ src/implements/payment.prismaRepository.ts
@@ -1,33 +1,33 @@
 import prisma from "../config/prisma.js";
 import { PaymentStatus, type Prisma } from "../generated/prisma/client.js";
 import type { PaymentRepository } from "../repositories/payment.repository.js";
-import type { PaymentInput } from "../types/payment.types.js";
+import type { PaymentInput, PaymentResponse } from "../types/payment.types.js";
 
 export class PrismaPaymentRepository implements PaymentRepository {
   async createPayment(tx:Prisma.TransactionClient, payment: PaymentInput): Promise<PaymentResponse> {
     
-    const paid_at = payment.status === PaymentStatus.PAID ? new Date() : null;
+    const paid_at = payment.payment_status === PaymentStatus.PAID ? new Date() : null;
 
     const created = await tx.payment.create({
       data: {
         order_id: payment.order_id,
         user_id: payment.user_id,
         total_amount: Number(payment.total_amount),
-        payment_method: payment.PaymentMethod,
+        payment_method: payment.payment_method,
         transaction_id: payment.transaction_id,
-        status: PaymentStatus,
-        paid_at:paid_at,
+        status: payment.payment_status,
+        paid_at:payment.paid_at,
       },
     });
 
     return {
       id: created.id,
       order_id: created.order_id,
-      user_id: created.userId || 0,
+      user_id: created.user_id || 0,
       total_amount: Number(created.total_amount),
       payment_method: created.payment_method,
       transaction_id: created.transaction_id || '',
-      status: created.PaymentStatus,
+      payment_status: created.status,
       paid_at: created.paid_at,
       created_at: created.created_at,
     };
@@ -35,7 +35,7 @@ export class PrismaPaymentRepository implements PaymentRepository {
 
   async findPaymentByOrderId(orderId: number): Promise<PaymentResponse | null> {
     const payment = await prisma.payment.findUnique({
-      where: { orderId },
+      where: { order_id:orderId },
     });
 
     if (!payment) return null;
@@ -47,7 +47,7 @@ export class PrismaPaymentRepository implements PaymentRepository {
       total_amount: Number(payment.total_amount),
       payment_method: payment.payment_method,
       transaction_id: payment.transaction_id || '',
-      status: payment.PaymentStatus,
+      payment_status: payment.status,
       paid_at: payment.paid_at,
       created_at: payment.created_at,
     };
