8915c7589cc1a28a1e963f9b11e73e81a61c7514
Gautam <gautam@gmail.com>
2026-07-15 19:40:46 +0530

    Prisma url missing issue solve

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 
-FROM node:20-alpine3.18
+FROM node:22-alpine
 
 WORKDIR /app
 
--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -1,27 +1,27 @@
-import type { NextFunction,Request,Response } from "express";
-import { addItemToCartService, getCartService, getOrCreateUserCartService, removeItemFromCartService, updateItemQuantityService } from "../services/cart.service.js";
+import type { NextFunction, Request, Response } from "express";
+import { addItemToCartService, getCartService, removeItemFromCartService, updateItemQuantityService } from "../services/cart.service.js";
 import { handleSuccessResponse } from "../utils/handleSuccessResponse.js";
 
 
-export const getCartController = async(req:Request, res:Response, next:NextFunction)=>{
-    try {
-        const userId = req.user.id;
-        const cartItems = await getCartService(userId)
-        return handleSuccessResponse(res, 200, "Cart Items fetched successfully", cartItems)
-    } catch (error) {
-        next(error)
-    }
+export const getCartController = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const userId = req.user.id;
+    const cartItems = await getCartService(userId)
+    return handleSuccessResponse(res, 200, "Cart Items fetched successfully", cartItems)
+  } catch (error) {
+    next(error)
+  }
 }
 
 export const addItemToCartController = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const userId = req.user.id;
-  
+
     const { product_id, quantity } = req.body;
 
     const item = await addItemToCartService(userId, product_id, quantity);
 
-    return handleSuccessResponse(res, 201, "Item added in cart successfully",item)
+    return handleSuccessResponse(res, 201, "Item added in cart successfully", item)
 
   } catch (error) {
     next(error);
@@ -48,12 +48,12 @@ export const updateItemController = async (req: Request, res: Response, next: Ne
 export const removeItemFromCartController = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const userId = req.user.id;
-   
+
     const itemId = Number(req.params.itemId)
 
-   const removedItem =  await removeItemFromCartService(userId, itemId);
+    const removedItem = await removeItemFromCartService(userId, itemId);
 
-   handleSuccessResponse(res, 200, "Item removed from cart", removedItem)
+    handleSuccessResponse(res, 200, "Item removed from cart", removedItem)
 
   } catch (error) {
     next(error);
