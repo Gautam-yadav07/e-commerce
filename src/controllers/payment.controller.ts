e197466e398be2b839cafc1e93ebd77536086e65
Gautam-yadav07 <ahirgautamyadav862004@gmail.com>
2026-07-21 00:13:47 +0530

    Added create razorpay order and verify payment controller

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -2,6 +2,8 @@ import type { Request, Response, NextFunction } from "express";
 import {
   processPaymentService,
   getPaymentDetailsService,
+  createRazorPayOrderService,
+  verifyPaymentService,
 } from "../services/payment.service.js";
 import { handleSuccessResponse } from "../utils/handleSuccessResponse.js";
 
@@ -38,6 +40,35 @@ export const getPaymentDetailsController = async (
     const payment = await getPaymentDetailsService(userId, orderId);
 
     handleSuccessResponse(res, 200, "Payment details fetched successfully", payment)
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const createRazorpayOrderController = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const user_id = req.user!.id;
+    const { order_id } = req.body;
+
+    const razorpayOrderData = await createRazorPayOrderService(user_id, order_id);
+
+    handleSuccessResponse(res, 201, "Razorpay order created successfully", razorpayOrderData)
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const verifyPaymentController = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const userId = req.user!.id;
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature, order_id } = req.body;
+    const data = {...req.body}
+
+    const payment = await verifyPaymentService(data);
+
+    handleSuccessResponse(res, 200, "Payment verified Successfully", payment)
 
   } catch (error) {
     next(error);
