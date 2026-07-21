ec2c7b423f365d1bfe8f9465068892c1ce7bdec6
Gautam <gautam@gmail.com>
2026-07-21 19:20:29 +0530

    Define routes for webhook

--- src/routes/payment.route.ts
+++ src/routes/payment.route.ts
@@ -1,12 +1,14 @@
-import { Router } from 'express';
+import express from 'express';
 import { authentication } from '../middlewares/authentication.middleware.js';
-import { createRazorpayOrderController, getPaymentDetailsController, processPaymentController, verifyPaymentController } from '../controllers/payment.controller.js';
+import { createRazorpayOrderController, getPaymentDetailsController, paymentWebhookController, processPaymentController, verifyPaymentController } from '../controllers/payment.controller.js';
 import { validator } from '../middlewares/validator.middleware.js';
 import { processPaymentSchema } from '../validators/payment.schema.js';
 import { authorization } from '../middlewares/authorization.middleware.js';
 
 
-const router = Router();
+const router = express.Router();
+router.post("/webhook",
+  express.raw({ type: "application/json" }), paymentWebhookController);
 
 router.use(authentication);
 router.use(authorization("CUSTOMER"))
@@ -16,5 +18,6 @@ router.get('/:orderId', getPaymentDetailsController);
 
 router.post("/order", createRazorpayOrderController);
 router.post("/verify", verifyPaymentController)
+
 
 export default router;
